55c4ca437e7df093c20fa8e8d627608654ab71b9
Subhasmita Sahoo <ssmita@ssmita-macbookpro.roam.corp.google.com>
2020-03-21 13:41:45 +0530

    2-keys-keyboard solution

---
+++ 2-keys-keyboard-sol1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minSteps(int n) {
+        int res = 0, d= 2;
+        
+        while(n>1) {
+            while(n%d == 0) {
+                res += d;
+                n/=d;
+            }
+            d++;
+        }
+        return res;
+    }
+}
