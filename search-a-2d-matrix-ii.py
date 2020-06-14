a11f4d5111d61ebc856f3fd14d4d226fdf52f5cd
Subhasmita Sahoo <subhasmitasahoo.247@gmail.com>
2020-06-14 19:47:12 +0530

    Create search-a-2d-matrix-ii.py

---
+++ search-a-2d-matrix-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        rlen = len(matrix)
+        if rlen==0:
+            return False
+        clen = len(matrix[0])
+        
+        i,j = 0,clen-1
+        while i<rlen and j>=0:
+            if matrix[i][j] == target:
+                return True
+            if matrix[i][j] < target:
+                i+=1
+            else:
+                j-=1
+        return False
+        
