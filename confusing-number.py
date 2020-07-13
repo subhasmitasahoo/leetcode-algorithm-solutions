c5b79eb02bfb8231aafc8b4bce1af32bb6fcf972
Subhasmita Sahoo <subhasmitasahoo.247@gmail.com>
2020-07-13 14:01:52 +0530

    Create confusing-number.py

---
+++ confusing-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def confusingNumber(self, N: int) -> bool:
+        reflect = {0:0,1:1,6:9,8:8,9:6}
+        temp = str(N)
+        length = len(temp)
+        
+        ne = False
+        for i in range(ceil(length/2)):
+            if int(temp[i]) not in reflect or int(temp[length-i-1]) not in reflect:
+                return False
+            if reflect[int(temp[i])] != int(temp[length-i-1]):
+                ne = True
+        
+        return ne
+        
+        
+        
